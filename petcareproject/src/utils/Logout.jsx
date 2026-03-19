c0df09f15a3dee6cd3d2f481e3f426be2b42826a
gopi dhameliya <gopidhameliya2804@gmail.com>
2026-03-19 09:56:36 +0530

    second commit

--- petcareproject/src/utils/Logout.jsx
+++ petcareproject/src/utils/Logout.jsx
@@ -1,25 +1,22 @@
-import cookie from "js-cookie";
-import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 
-function Logout () {
-    const navigate = useNavigate();
-    try{
-        cookie.remove("token");
-        alert("Logout Successful")
-        navigate("/login");
-    } catch (e) {
-        console.log(e);
-    }
+function Logout(navigate) {
+  try {
+    Cookies.remove("token");
+    alert("Logout Successful");
+    navigate("/login");
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-function LogoutWithoutNotification() {
-    const navigate = useNavigate();
-    try{
-        cookie.remove("token");
-        navigate("/login");
-    } catch (e) {
-        console.log(e);
-    }
+function LogoutWithoutNotification(navigate) {
+  try {
+    Cookies.remove("token");
+    navigate("/login");
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default Logout;
