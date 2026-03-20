b5e3ae239a1307fb8eed51d0de190ad59e7e9cf8
gopi dhameliya <gopidhameliya2804@gmail.com>
2026-03-20 10:54:53 +0530

    second commit

--- petcareproject/src/App.jsx
+++ petcareproject/src/App.jsx
@@ -89,7 +89,7 @@ function App() {
         </Routes>
         <Footer />
       </BrowserRouter>
-      <ToastContainer position="top-right" autoClose={8000} />
+      <ToastContainer position="top-right" autoClose={3000} theme="colored"/>
     </>
   )
 }
