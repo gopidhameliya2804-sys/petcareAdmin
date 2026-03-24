d5decb7fc76d26e45abc519e40ccb4ec0080e253
gopi dhameliya <gopidhameliya2804@gmail.com>
2026-03-24 12:27:42 +0530

    second commit

--- petcareproject/src/main.jsx
+++ petcareproject/src/main.jsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from 'react-toastify';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StrictMode } from 'react';
 
 const queryClient = new QueryClient();
 
