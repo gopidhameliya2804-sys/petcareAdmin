a8004c2a007f7e7056d90f6a0e9254a68cdb3258
gopi dhameliya <gopidhameliya2804@gmail.com>
2026-03-25 10:10:01 +0530

    second commit

--- petcareproject/src/pages/ServiceCategory.jsx
+++ petcareproject/src/pages/ServiceCategory.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import BreadCrumbs from "../comman/BreadCrumbs";
 import api from "../utils/AxiosConfig";
+import { useQuery } from "@tanstack/react-query";
 
 function ServiceCategory() {
   // let [serviceCategory, setServiceCategory] = useState({});
--- petcareproject/src/pages/Services.jsx
+++ petcareproject/src/pages/Services.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import BreadCrumbs from "../comman/BreadCrumbs";
 import api from "../utils/AxiosConfig";
+import { useQuery } from "@tanstack/react-query";
 
 function Services() {
   // let [services, setServices] = useState({});
