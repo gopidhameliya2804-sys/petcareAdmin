73a6e7f04269840944371d39d62ab8d987df88d2
gopi dhameliya <gopidhameliya2804@gmail.com>
2026-04-02 10:20:25 +0530

    second commit

--- petcarebackend/src/app.js
+++ petcarebackend/src/app.js
@@ -37,7 +37,7 @@ let Feedback = require("./routes/admin/feedback.route");
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin:[ "https://petcareadmin.onrender.com", "https://petcareuser.onrender.com" , "http://localhost:5173", "http://localhost:5174"],
+    origin:[ "https://petcareadmin.onrender.com", "https://petcareuser.onrender.com"],
     methods: ["GET" , "POST" , "PUT" ,"DELETE"],
     credentials: true
 }))
